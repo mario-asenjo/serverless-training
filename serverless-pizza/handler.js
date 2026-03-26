96807f0e9c332f6249e1cde1346c21d738a3c23d
root <root@vmi3049032.contaboserver.net>
2026-03-26 10:52:44 +0100

    Added first two apps for serverless framework with AWS

---
+++ serverless-pizza/handler.js
@@ -0,0 +1,28 @@
+const { v4: uuidv4 } = require('uuid');
+
+exports.newOrder = async (event) => {
+	const orderId = uuidv4();
+	console.log(orderId);
+
+	let orderDetails;
+	try {
+		orderDetails = JSON.parse(event.body);
+	} catch (error) {
+		console.error("Error parsing order details:", error);
+		return {
+			statusCode: 400,
+			body: JSON.stringify({ message: "Invalid JSON format in order details" })
+		}
+	}
+
+	console.log(orderDetails)
+
+	const order = {orderId, ...orderDetails}
+
+	return {
+		statusCode: 200,
+		body: JSON.stringify({
+			message: order
+		})
+	};
+}
